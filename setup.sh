f1bd4c872ba742e77b3e719c4febe7bec2717699
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2019-09-23 16:39:43 +0100

    source .zsh files in subdirectories

--- setup.sh
+++ setup.sh
@@ -3,27 +3,26 @@
 set -e
 
 sym_file() {
-	if [ -e "$2" ]; then
-		if [ "$(readlink "$2")" = "$1" ]; then
-			return 0
-		fi
-	fi
+    if [ -e "$2" ]; then
+        if [ "$(readlink "$2")" = "$1" ]; then
+            return 0
+        fi
+    fi
 
-	ln -sf "$1" "$2"
+    ln -sf "$1" "$2"
 }
 
 sym_dots() {
-	find -H "$(pwd -P)" -maxdepth 3 -name "*.symlink" -not -path "*.git" |
-		while read -r src; do
-			dest="$HOME/.$(basename "${src%.*}")"
-			sym_file "$src" "$dest"
-		done
-}
+    find -H "$(pwd -P)" -maxdepth 3 -name "*.symlink" -not -path "*.git" |
+        while read -r src; do
+            dest="$HOME/.$(basename "${src%.*}")"
+            sym_file "$src" "$dest"
+        done
+    }
 
 sym_dots
 
 git ls-tree --name-only -r HEAD | grep install.sh | while read -r installer; do
-    echo "> ${installer}..."
     chmod +x $installer
     sh -c "$installer"
 done
--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 vim +'PlugInstall --sync' +qa
 vim +'PlugUpdate' +qa
