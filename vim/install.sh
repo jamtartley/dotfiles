d96faf81ee6650bc95a75c7d9011ee8eb1b3be06
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2019-09-24 06:36:08 +0100

    Add post-install step for YouCompleteMe

--- vim/install.sh
+++ vim/install.sh
@@ -4,6 +4,14 @@ if [[ $(uname -s) == *"Linux"* ]];
 then
     echo "Installing vim..."
     sudo $PACKAGE_MANAGER install vim
+
+    if [[ $PACKAGE_MANAGER == "dnf" ]]
+    then
+        sudo dnf install cmake gcc-c++ make python3-devel
+    elif [[ $PACKAGE_MANAGER == "apt-get" ]]
+    then
+        sudo apt-get install build-essential cmake python3-dev
+    fi
 fi
 
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
@@ -11,3 +19,5 @@ curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
 
 vim +'PlugInstall --sync' +qa
 vim +'PlugUpdate' +qa
+
+python3 $HOME/.vim/plugged/youcompleteme/install.py --ts-completer --clang-completer
