57639a75308f9ccff13ce8e3894fe19ac667615f
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2019-09-26 09:33:40 +0100

    Add OmniSharp for C# in vim

--- vim/install.sh
+++ vim/install.sh
@@ -17,7 +17,6 @@ fi
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim +'PlugInstall --sync' +qa
-vim +'PlugUpdate' +qa
+vim +'PlugClean' +'PlugInstall --sync' +'PlugUpdate' 'OmniSharpInstall' +qa
 
-python3 $HOME/.vim/plugged/youcompleteme/install.py --ts-completer --clang-completer
+python3 $HOME/.vim/plugged/youcompleteme/install.py --ts-completer --clang-completer --omnisharp-completer
