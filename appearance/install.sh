020766dacd27f39ed69dd87b5302b2ddc6c6b22f
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2019-10-03 06:46:10 +0100

    Install Papirus font

--- appearance/install.sh
+++ appearance/install.sh
@@ -7,4 +7,16 @@ wget https://github.com/EliverLara/Ant-Dracula/releases/download/v1.3.0/Ant-Drac
 tar -xvf $HOME/.themes/Ant-Dracula.tar -C $HOME/.themes/
 rm $HOME/.themes/Ant-Dracula.tar
 
+FONTS=$HOME/.fonts
+mkdir -p $FONTS
+pushd $FONTS
+wget https://www.levien.com/type/myfonts/Inconsolata.otf -P $FONTS
+wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
+unzip master.zip
+mv YosemiteSanFranciscoFont-master/*.ttf $FONTS
+rm -rf YosemiteSanFranciscoFont-master master.zip
+popd
+
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | DESTDIR="$HOME/.icons" sh
+
 ln -sf $(pwd)/.gtkrc-2.0.mine $HOME
