c234c83be8d559a4cbce527d24188638e388633a
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2019-10-19 18:44:00 +0100

    Add seperate file for graphical stuff

---
+++ graphics.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+sudo dnf install -y fedora-workstation-repositories
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager -y --set-enabled rpmfusion-nonfree-nvidia-driver
+sudo dnf config-manager -y --set-enabled rpmfusion-nonfree-steam
+sudo dnf install -y steam nvidia-driver akmod-nvidia
