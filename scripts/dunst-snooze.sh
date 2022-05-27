6c07ccde65d8185bf53f3c6c5940e2df9e1cba2d
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2022-05-27 07:16:56 +0100

    feat: add package update counts and dunst snoozer scripts

---
+++ scripts/dunst-snooze.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+case "$1" in
+    --toggle)
+        dunstctl set-paused toggle
+        ;;
+    *)
+        if [ "$(dunstctl is-paused)" = "true" ]; then
+            echo ""
+        else
+            echo ""
+        fi
+        ;;
+esac
---
+++ scripts/pacman_updates.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
+    updates=0
+fi
+
+if [ "$updates" -gt 0 ]; then
+    echo " $updates"
+else
+    echo ""
+fi
