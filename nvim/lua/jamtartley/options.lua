9442842638c013079515276d139b5a8691d4b8ec
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2022-05-28 22:29:12 +0100

    feat: write custom foldtext render function

--- nvim/lua/jamtartley/options.lua
+++ nvim/lua/jamtartley/options.lua
@@ -1,57 +1,64 @@
+function _G.foldtext()
+	local lines = vim.v.foldend - vim.v.foldstart + 1
+	local first = vim.fn.getline(vim.v.foldstart)
+
+	return "··[" .. lines .. "ℓ]··" .. first
+end
+
 local options = {
-  backup = false,
-  clipboard = "unnamedplus",
-  cmdheight = 2,
-  completeopt = { "menuone", "noselect" },
-  conceallevel = 0,
-  cursorline = true,
-  expandtab = false,
-  fileencoding = "utf-8",
+	backup = false,
+	clipboard = "unnamedplus",
+	cmdheight = 2,
+	completeopt = { "menuone", "noselect" },
+	conceallevel = 0,
+	cursorline = true,
+	expandtab = false,
+	fileencoding = "utf-8",
 	fillchars      = {
 		diff = '∙',
 		eob = ' ',
-		fold = '·',
+		fold = ' ',
 		vert = '┃',
 	},
-	foldenable = true,
-	foldlevelstart = 1,
+	foldlevelstart = 0,
 	foldmethod = "indent",
-  hlsearch = true,
-  ignorecase = true,
+	foldtext = 'v:lua.foldtext()',
+	hlsearch = true,
+	ignorecase = true,
 	list = true,
 	listchars = {
 		space = '•',
 		tab = '➜ ' 
 	},
-  mouse = "a",
-  number = true,
-  numberwidth = 1,
-  pumheight = 10,
-  relativenumber = true,
-  scrolloff = 8,
-  shiftwidth = 2,
-  showmode = false,
-  showtabline = 0,
-  sidescrolloff = 8,
-  signcolumn = "yes",
-  smartcase = true,
-  smartindent = true,
-  splitbelow = true,
-  splitright = true,
-  swapfile = false,
-  tabstop = 2,
-  termguicolors = true,
-  timeoutlen = 1000,
-  undofile = true,
-  updatetime = 300,
-  wrap = false,
-  writebackup = false,
+	mouse = "a",
+	number = true,
+	numberwidth = 1,
+	pumheight = 10,
+	relativenumber = true,
+	scrolloff = 8,
+	shiftwidth = 2,
+	showmode = false,
+	showtabline = 0,
+	sidescrolloff = 8,
+	signcolumn = "yes",
+	smartcase = true,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	swapfile = false,
+	tabstop = 2,
+	termguicolors = true,
+	timeoutlen = 1000,
+	undofile = true,
+	updatetime = 300,
+	wrap = false,
+	writebackup = false,
 }
 
 vim.opt.shortmess:append "c"
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
