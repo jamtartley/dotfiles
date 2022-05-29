f2f4412875bfca972d0e3c92b5b62966e9b5459c
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2022-05-29 10:06:34 +0100

    feat: symlink public tmuxinator project configs

--- nvim/lua/jamtartley/autocommands.lua
+++ nvim/lua/jamtartley/autocommands.lua
@@ -1,9 +1,16 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	pattern = "*",
-	callback = function()
-		require"vim.highlight".on_yank({
-			higroup = "DiffAdd",
-			timeout = 350
-		})
-	end
+		pattern = "*",
+		callback = function()
+				require"vim.highlight".on_yank({
+						higroup = "DiffAdd",
+						timeout = 350
+				})
+		end
 })
+
+vim.cmd [[
+		augroup yaml_fix
+				autocmd!
+				autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab indentkeys-=0# indentkeys-=<:>
+		augroup END
+]]
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 dir="$(dirname -- "$(readlink -f -- "$0")")"
 
 TMUX=$HOME/.config/tmux
+TMUXINATOR=$HOME/.config/tmuxinator
 mkdir -p "$TMUX"
+mkdir -p "$TMUXINATOR"
 ln -sf "$dir/tmux.conf" "$TMUX"
+ln -sf "$dir"/tmuxinator/* "$TMUXINATOR/"
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
