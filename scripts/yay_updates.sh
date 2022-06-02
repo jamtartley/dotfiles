5d036f22f9b49e8fc3b2768456801e985f24f1ef
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2022-06-03 00:11:19 +0100

    fix: package update outputs

--- scripts/yay_updates.sh
+++ scripts/yay_updates.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-UPDATES=$(yay -Qum | sed -r '/^\s*$/d')
-UPDATE_COUNT=$(echo -n $UPDATES | wc -l)
+UPDATES=$(yay -Qum)
+UPDATE_COUNT=$(echo $UPDATES | wc -l)
 
 if [ "$UPDATE_COUNT" -gt 0 ]; then
     echo " $UPDATE_COUNT"
