84bb3468f70688114a07e14efc594d3173a2c1b1
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2023-08-06 20:50:40 +0100

    chore: create xorg ansible role

--- tmux/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-dir="$(dirname -- "$(readlink -f -- "$0")")"
-
-TMUX=$HOME/.config/tmux
-mkdir -p "$TMUX"
-ln -sf "$dir/tmux.conf" "$TMUX"
--- xorg/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-dir="$(dirname -- "$(readlink -f -- "$0")")"
-ln -sf $dir/.Xresources $HOME
-ln -sf $dir/.xinitrc $HOME
