da309556a1a103ba1a82360fd4f3c3f626d52e47
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2023-08-06 21:15:31 +0100

    chore: add polybar role

--- polybar/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-dir="$(dirname -- "$(readlink -f -- "$0")")"
-
-POLYBAR="$HOME/.config/polybar"
-mkdir -p "$POLYBAR"
-ln -sf "$dir/config.ini" $POLYBAR/
-ln -sf "$dir/launch.sh" $POLYBAR/
