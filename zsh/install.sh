6b2724b159bc34e2ea1aeea45d7ccbe54f2b8ae7
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2023-08-06 21:53:15 +0100

    chore: add zsh role

--- zsh/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-dir="$(dirname -- "$(readlink -f -- "$0")")"
-
-ln -sf $dir/.zshrc $HOME/.zshrc
-mkdir -p $HOME/.zsh
-ln -sf $dir/*.zsh $HOME/.zsh
