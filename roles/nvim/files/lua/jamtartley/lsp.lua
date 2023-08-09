62312946a649032bfd46f3ce7d14929aa0d0b8be
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2023-08-09 19:24:10 +0100

    chore: add brew bundle on pre-commit

--- roles/nvim/files/lua/jamtartley/lsp.lua
+++ roles/nvim/files/lua/jamtartley/lsp.lua
@@ -54,17 +54,3 @@ lsp.format_on_save({
 })
 
 lsp.setup()
-
-local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-if not null_ls_status_ok then
-	return
-end
-
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.code_actions.eslint_d,
-		null_ls.builtins.diagnostics.eslint_d,
-	},
-})
