969de7dc4dbd7d32bdab8388aa8e651f7023f389
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2024-03-17 15:40:58 +0000

    chore(nvim): add some setup config for Neogit

--- roles/nvim/files/lua/jamtartley/neogit.lua
+++ roles/nvim/files/lua/jamtartley/neogit.lua
@@ -2,7 +2,20 @@ local neogit = require("neogit")
 
 neogit.setup({
 	disable_commit_confirmation = true,
+	disable_hint = true,
+	disable_signs = true,
+	highlight = {
+		italic = true,
+		bold = true,
+		underline = true,
+	},
+	git_services = {
+		["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+		["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+	},
+	graph_style = "unicode",
 	integrations = {
 		diffview = true,
 	},
+	remember_settings = false,
 })
