1b0183a83eca8e288d1770aff7338cbbaab2a265
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2025-01-06 16:05:02 +0000

    chore(nvim): add icons to blink menu

--- roles/nvim/files/lua/config/plugins/completion.lua
+++ roles/nvim/files/lua/config/plugins/completion.lua
@@ -6,10 +6,56 @@ return {
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
+			kind_icons = {
+				Copilot = "",
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
+
+				Field = "󰜢",
+				Variable = "󰆦",
+				Property = "󰖷",
+
+				Class = "󱡠",
+				Interface = "󱡠",
+				Struct = "󱡠",
+				Module = "󰅩",
+
+				Unit = "󰪚",
+				Value = "󰦨",
+				Enum = "󰦨",
+				EnumMember = "󰦨",
+
+				Keyword = "󰻾",
+				Constant = "󰏿",
+
+				Snippet = "󱄽",
+				Color = "󰏘",
+				File = "󰈔",
+				Reference = "󰬲",
+				Folder = "󰉋",
+				Event = "󱐋",
+				Operator = "󰪚",
+				TypeParameter = "󰬛",
+			},
 		},
 		completion = {
-			documentation = { window = { border = "single" } },
-			menu = { border = "single" },
+			documentation = {
+				window = {
+					border = "single",
+				},
+			},
+			menu = {
+				border = "single",
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind", gap = 1 },
+					},
+					treesitter = { "lsp" },
+				},
+			},
 		},
 		keymap = {
 			preset = "enter",
@@ -19,7 +65,12 @@ return {
 				end,
 			},
 		},
-		signature = { enabled = true, window = { border = "single" } },
+		signature = {
+			enabled = true,
+			window = {
+				border = "single",
+			},
+		},
 		sources = {
 			cmdline = {},
 			default = { "lsp", "path", "buffer", "copilot" },
@@ -27,8 +78,16 @@ return {
 				copilot = {
 					name = "copilot",
 					module = "blink-cmp-copilot",
-					score_offset = 100,
 					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
 				},
 			},
 		},
