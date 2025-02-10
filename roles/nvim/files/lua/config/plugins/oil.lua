6c9a11ba5824d302ae896c7b0aad77434659b58f
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2025-02-10 08:18:26 +0000

    fix(zsh): after updating asdf

--- roles/nvim/files/lua/config/plugins/oil.lua
+++ roles/nvim/files/lua/config/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	config = function()
-		require("oil").setup({})
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 		vim.keymap.set("n", "-", "<cmd>Oil<cr>")
 	end,
 }
